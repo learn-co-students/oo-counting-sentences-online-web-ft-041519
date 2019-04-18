708e604b5ee2c32b992ad2622b6394b86ee62a00
James Pier <jpier2012@gmail.com>
2019-04-18 13:41:12 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,26 @@
 require 'pry'
 
 class String
+  def initialize(name)
+    @name = name
+  end
+
+  attr_reader :name
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = self.split(/[\!\.\?]+\s*/)
+    sentences.length
   end
 end
