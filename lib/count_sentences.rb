c5510d0943cf78a01e4c2448f50e7e50b16ac674
Annia Forney <anniaforney@gmail.com>
2019-04-25 20:13:12 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    "Hi".end_with?("!")
+    => true 
   end
 
   def question?
-
+    "How are you?".self_with?("?")
   end
 
   def exclamation?
-
+    "YO!!!".self_with?("!!!")
   end
 
   def count_sentences
-
+    
   end
 end
