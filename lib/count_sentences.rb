e4b57dcbd9112b3b8323b87fbb4f165698421bf1
Garth Lilly <garff247@gmail.com>
2019-04-26 03:14:05 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,36 @@
 require 'pry'
 
 class String
+#attr_reader :sentence?, :question?, :exclamation?, :count_sentences
 
   def sentence?
-    
+    if self.end_with?(".") == true
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?") == true
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!") == true
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
+    array = self.split(/\.+|\?+|!+/)
+    return array.length
+
 
   end
 end
