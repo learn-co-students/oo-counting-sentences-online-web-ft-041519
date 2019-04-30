bfdd8d01ea2ee239a12b77bd9e93de41135ae76b
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-30 01:14:49 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,8 +16,7 @@ class String
   
   def count_sentences
     self.squeeze('.!?').count('.!?') 
-    
-  
-    
   end
+# .SQUEEZE REMOVES THE EXTRA .?! FROM THE COMPLEX SENTANCE EX: !!! MAKING IT POSSIBLE TO RUN .COUNT ON THE REMAINING.
+    
 end
